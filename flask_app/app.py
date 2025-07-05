72acf0c121d7fed54f196b50d6db778972f8aabf
samclintonb <clintonsam05@gmail.com>
2025-07-05 16:23:29 +0000

    updating app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Docker configured with Ansible!"
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    return render_template('thanks.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
